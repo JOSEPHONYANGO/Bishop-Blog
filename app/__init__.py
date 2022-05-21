a5fa63445aa53f73a35c6827c7c38d52881d45a8
JOSEPHONYANGO <joseph.onyango@student.moringaschool.com>
2022-05-21 22:19:13 +0300

    ref

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,26 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_uploads import UploadSet, configure_uploads, IMAGES
+# from flask_uploads import UploadSet, configure_uploads, IMAGES
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
-from flask_admin import Admin
+# from flask_admin import Admin
 from config import config_options
 
-db = SQLAlchemy()
-login_manager = LoginManager()
+app = Flask(__name__)
+
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
 login_manager.session_protection = "strong"
 login_manager.login_view = "auth.login"
-photos = UploadSet("photos", IMAGES)
-mail = Mail()
+# photos = UploadSet("photos", IMAGES)
+mail = Mail(app)
 bootstrap = Bootstrap()
-admin = Admin()
+# admin = Admin()
 
 def create_app(config_name):
     
-    app = Flask(__name__)
+    # app = Flask(__name__)
 
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
@@ -28,7 +30,7 @@ def create_app(config_name):
     login_manager.init_app(app)
     mail.init_app(app)
     bootstrap.init_app(app)
-    admin.init_app(app)
+    # admin.init_app(app)
 
     # Registering the main app Blueprint
     from .main import main as main_blueprint
@@ -39,9 +41,9 @@ def create_app(config_name):
     app.register_blueprint(auth_blueprint, url_prefix = "/authenticate")
     
     # Configure UploadSet
-    configure_uploads(app, photos)
+    # configure_uploads(app, photos)
 
-    return app
+    # return app
 
 
     app.run(port=3003,host='0.0.0.0')
